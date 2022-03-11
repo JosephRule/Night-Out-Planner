0ec6c201a2c8ee164155eb92f28cfe795087c743
Yunzhu Jin <jyz022020@gmail.com>
2022-03-10 22:57:41 -0800

    add api call

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function getETAInfo(start, end) {
+    var proxyurl = "https://cors-anywhere.herokuapp.com/";
+    var directionInfo = fetch("https://maps.googleapis.com/maps/api/directions/json?origin=" + start + "&destination=" + end + "&key=" + "AIzaSyCmFDQf58ZuTf-R93Rkrlxk5HEeNe2XmjY");
+    directionInfo.then(directionInfo => {
+        console.log(directionInfo);
+        // var distance = directionInfo.routes.legs[0].distance.text;
+        // var duration = directionInfo.routes.legs[0].duration.text;
+    })
+}
+
+getETAInfo("Disneyland","Universal+Studios+Hollywood");
